4b23fde9558d6537d5b007dd56fe6250add2791a
doomsday4 <amankhilani.ajmer@gmail.com>
2023-06-04 04:47:11 +0530

    extension.ts updated

--- lasteditor/src/extension.ts
+++ lasteditor/src/extension.ts
@@ -1,41 +1,59 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+import { exec } from 'child_process';
 
-export function activate(context: vscode.ExtensionContext) {
+export {
+  activate,
+  deactivate
+};
 
-  let disposable = vscode.commands.registerCommand('extension.showLastEditor', async () => {
-    let outputChannel: vscode.OutputChannel;
-    outputChannel = vscode.window.createOutputChannel('Last Editor');
+function activate(context: vscode.ExtensionContext) {
 
-    const gitExtension = vscode.extensions.getExtension('vscode.git');
-    if (gitExtension) {
+  let disposable = vscode.commands.registerCommand("extension.showLastEditor", () => {
 
-      const git = gitExtension.exports.getAPI(1);
-      const repositories = git.repositories;
-      if (repositories.length > 0) {
+    const editor = vscode.window.activeTextEditor; // To access the active Text Editor in VS Code
 
-        const activeEditor = vscode.window.activeTextEditor;
-        if (activeEditor) {
-          const filePath = activeEditor.document.uri.fsPath;
-          const blameInfo = await repositories[0].blame(filePath);
-          if (blameInfo && blameInfo.length > 0) {
+    if (editor) {
+      const filePath = editor.document.uri.fsPath; // To access the file path of the document/file currently open
+      const absolutefilePath = path.resolve(filePath);
+      const directoryPath = path.dirname(absolutefilePath);
+      console.log(directoryPath);
+      const fileName = path.basename(absolutefilePath);
+      console.log(fileName);
 
-            for (let i = 0; i < blameInfo.length; i++) {
-              const line = i + 1; // Lines are 1-based, so increment the index by 1
-              const lastEditAuthor = blameInfo[i].author.name;
-              outputChannel.appendLine(`Last author at line ${line}: ${lastEditAuthor}`);
-            }
-          }
-        } else {
-          vscode.window.showWarningMessage('No active text editor found.');
+      const selection = editor.selection; // This part is specifically to display the username of the last editor
+      const lineNumber = selection.active.line; // for the current line in the code.
+
+      const options = { cwd: directoryPath };
+      // This command uses the Git Blame info to get the author name for the current line and the --porcelain command is specifically used 
+      // to avoid displaying the date and time of the last edit. Hence it displays only the username.
+      exec(`git blame -L ${lineNumber + 1},${lineNumber + 1} --porcelain ${fileName}`, options, (error: Error | null, stdout: string) => {
+        if (error) {
+          console.error(`Error executing "git blame": ${error}`);
+          return;
         }
-      } else {
-        vscode.window.showWarningMessage('No Git repository found in the workspace.');
-      }
-    } else {
-      vscode.window.showWarningMessage('The Git extension is not installed.');
+
+        const match = /^author (.+)$/m.exec(stdout);
+        console.log(match?.[1]); // To avoid the error because of match = NULL, we difine that match is never = NULL, using the ?
+        if (match) {
+          const author = match[1];
+          const range = new vscode.Range(lineNumber, 0, lineNumber, 0);
+          const decoration = vscode.window.createTextEditorDecorationType({
+            color: '#FF0000',
+            after: {
+              contentText: `[${author}]`, // If the last changes are not yet pushed on git, it displays NOT COMMITTED YET
+              color: 'red' // Changed the color to red so that it is easily visible and not confusing with the code.
+            }
+          });
+          editor.setDecorations(decoration, [range]);
+        }
+      });
     }
-    outputChannel.show();
   });
 
   context.subscriptions.push(disposable);
+}
+
+function deactivate() {
+  console.log('The LastEditor extension is deactivated.');
 }
